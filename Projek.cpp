63c02b6e78e384ae7601a2a1a13b98fcf3ebbe59
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-06 15:43:13 +0700

    membuat void bubble sort dan voit display

---
+++ Projek.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input()
+{
+    int d;
+    while (true)
+    {
+        cout << "masukkan banyaknya elemn dalam array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Error : Maksimal input array adalah 20\n";
+        }
+    }
+
+     cout << endl;
+    cout << "===================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "===================" << endl;
+
+      for (int i = 0; i < n; i++)
+    {
+    cout << "Masukkan Nilai ke- " << i + 1 << ": ";
+    cin >> a[i];
+    }
+}
+
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "element array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+         cout << "Data ke - " << i << " : " << a[i] << endl;
+    }
+}
+
+void bubleSort()
+{
+    cout << "\n===============================" << endl;
+    cout << "Hasil setiap Pass" << endl;
+    cout << "===============================" << endl;\
+    for (int pass = 1; pass <n; pass++)
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j +1];
+                a[j + 1] = temp;
+            }
+        }
+
+        cout << "Pass ke - " << pass << ":\t";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << "\t ";
+        }
+        cout << endl;
+    }
+}
